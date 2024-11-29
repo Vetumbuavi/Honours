6b78cee341ad0ac1bfba68c78045122a58fcbc7e
vetu <v2ndjavera@gmail.com>
2024-11-29 15:29:22 +0200

    removed folder

--- jobhubv2/android/app/src/main/kotlin/com/example/jobhubv2_0/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jobhubv2_0
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- jobhubv2/lib/views/common/exports.dart
+++
@@ -1,3 +0,0 @@
-export 'package:jobhubv2_0/constants/app_constants.dart';
-export 'package:jobhubv2_0/views/common/app_style.dart';
-export 'package:jobhubv2_0/views/common/reusable_text.dart';
