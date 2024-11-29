047716dd3e0052648a44c5edbc4945954656d5b7
vetu <v2ndjavera@gmail.com>
2024-11-29 16:30:57 +0200

    Setup files

--- jobhubv2/lib/main.dart
+++ jobhubv2/lib/main.dart
@@ -14,11 +14,10 @@ import 'package:jobhubv2_0/views/screens/mainscreen.dart';
 import 'package:jobhubv2_0/views/screens/onboarding/onboarding_screen.dart';
 import 'package:provider/provider.dart';
 
-Widget defaultHome = const OnboardingScreen();
+Widget defaultHome = const Mainscreen();
+
 ///TODO: Hook the app to firebase using firebase cli
 void main() async {
- 
-
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (context) => OnBoardNotifier()),
     ChangeNotifierProvider(create: (context) => LoginNotifier()),
@@ -29,6 +28,7 @@ void main() async {
     ChangeNotifierProvider(create: (context) => ProfileNotifier()),
   ], child: const MyApp()));
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
         builder: (context, child) {
           return GetMaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'Dbestech JobHub',
+            title: 'Livestock Marketing App',
             theme: ThemeData(
               scaffoldBackgroundColor: Color(kLight.value),
               iconTheme: IconThemeData(color: Color(kDark.value)),
